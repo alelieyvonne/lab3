fb8711e0c8d73591a90cba81958d17c1ec219363
alelieyvonne <118198057+alelieyvonne@users.noreply.github.com>
2023-03-10 01:21:09 +0800

    Add files via upload

---
+++ ci4/app/Views/pages/about.php
@@ -0,0 +1,437 @@
+<html>
+    <head>
+        <title> Website </title>
+        <link rel="stylesheet" href="style1.css"> </link>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <style>
+.error {color: #FF0000;}
+        body{
+            margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+}
+.banners{
+    width: 100%;
+    height: 100%;
+    background-image: url(images/background.png.jpg);
+    background-size: cover;
+    background-position: center;
+}
+
+html, body {
+  height: 100%;
+  margin: 0;
+  font-family: Lato;
+  color: rgb(123, 85, 85);
+}
+
+#page1 { background: #f6e0b5; }
+
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage1 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled1 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column1 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 50px;
+    padding: 10px;
+    width: 40%;
+    min-height: 500px;
+    background-size: 20px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h1 {
+  font-size: 20px;
+  font-style: italic;
+  position: relative;
+  margin-left: 25%;
+  margin-top: 10%;
+  color:rgb(118, 79, 79);
+  text-align: center;
+}
+
+p, a{
+  margin-bottom: 20%;
+  margin-right: 20%;
+  font-size: 20px;
+  font-style: normal;
+  color: #babc9a;
+  text-align: center;
+}
+
+
+.column2 {
+    float: right;
+    display: flex;
+    background: #dfe0cc;
+    margin-right: 50px;
+    padding: 10px;
+    width: 40%;
+    min-height: 500px;
+    border: 5px rgb(94, 68, 68) solid;
+}
+img {
+  width: 100%;
+  height: 100%;
+  margin-top: 70px;
+  display: block;
+  margin: auto;
+}
+
+
+#page2 { background: #efc1b0; }
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage2 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled2 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column3 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 5px;
+    margin-right: 0;
+    padding: 5px;
+    width: 97%;
+    max-height: 100%;
+    background-size: 5px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h2{
+  position: relative;
+  align-items: center;
+  text-align: center;
+}
+
+p, a{
+  position: relative;
+  text-align: center;
+}
+
+#page3 { background: #aa6f73; }
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage3 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled3 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column4 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 5px;
+    margin-right: 0;
+    padding: 5px;
+    width: 97%;
+    max-height: 100%;
+    background-size: 5px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h2{
+  position: relative;
+  align-items: center;
+  text-align: center;
+}
+
+p, a{
+  position: relative;
+  text-align: center;
+}
+
+
+
+</style>
+
+
+
+
+
+    <body>
+        <container class="container">
+
+     
+          <div class="wrapper tabled">
+  <div class="stage1" id="page1">
+    <div class="middled1">
+
+        <div class="row1">
+            <div class="column1">
+              <h1> 
+                ALELIE YVONNE TAYCO<br><br>
+                BSCS-SF211<br><br>
+               WEBPROG WEBSITE <br> <br>
+
+               <p>
+                <a href="https://www.facebook.com/alelietayco/" target="_blank" style="color: #3b5998;">FACEBOOK</a><br><br>
+                <a href="https://www.instagram.com/alelieyvonne/" target="_blank" style="color: #d62976;">INSTAGRAM</a><br><br>
+                <a href="https://twitter.com/alelieyvonne" target="_blank" style="color: #1DA1F2;">TWITTER</a><br><br>
+                <a href="https://www.linkedin.com/in/alelie-yvonne-tayco-42809720a/" target="_blank" style="color: #006192;">LINKEDIN</a><br><br>
+                <a href="https://outlook.office.com/mail/" target="_blank" style="color: #249ee4;">OUTLOOK</a><br><br>
+               </p>
+
+              
+              </h1>
+            </div>
+
+            <div class="row">
+                <div class="column2"> 
+                
+                
+                
+                   
+
+                </div>
+            </div>
+
+          </div>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="wrapper tabled">
+  <div class="stage2" id="page2">
+    <div class="middled2">
+      
+      <div class="row2">
+        <div class="column3">
+          <p> jshusjshjshjk </p>
+          
+          <div class="card">
+           
+         </div>
+
+        </div>
+        </div>
+
+      
+
+    </div>
+  </div>
+</div>
+
+
+<div class="wrapper tabled">
+    <div class="stage3" id="page3">
+      <div class="middled">
+
+      <div class="row3">
+        <div class="column4">
+        <div class="card">
+
+        
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "webprogss211db";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
+	VALUES ('$name','$email','$website','$comment','$gender')";
+	
+	if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+	} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	$conn->close();	
+	
+}
+?>
+         </div>
+      </div>
+    </div>
+  </div>
+
+        </container>
+    </body>
+</html>
---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Website </title>
+        <meta name="viewport" conten="width=device-width, initial-scale=1">
+        
+    <style>
+    body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    
+}
+.banner{
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    <img src
+}
+.content{
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    text-align: center;
+    color: azure;
+}
+.content h1{
+    font-size: 50px;
+    margin-top: 80px;
+}
+.content p{
+    margin: 20px auto;
+    font-weight: 100;
+    line-height: 25px;
+}
+button{
+    width: 200px;
+    padding: 15px 0;
+    text-align: center;
+    margin: 20px 10px;
+    border-radius: 25px;
+    font-weight: bold;
+    border: 2px solid brown;
+    background: transparent;
+    color: #fff;
+    cursor: pointer;
+}
+
+
+
+        </style>
+         </head>
+         <body>
+
+         
+        <div class="banner">
+            
+            <div class="content">
+                <h1> Alelie Yvonne Tayco </h1>
+                <p>BSCS-SF</p>
+                <div>
+                    <button type="button"><a href="about" target="_blank" style="color: rgb(94, 68, 68);">Click Me!</a></button>
+                </div>
+            </div>
+        </div>
+</html>                                                                                                                                                                               
