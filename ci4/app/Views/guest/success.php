7df5a4d18cb586cdb672ee1dafc4d3c12ff93f21
alelieyvonne <118198057+alelieyvonne@users.noreply.github.com>
2023-03-17 19:04:28 +0800

    Add files via upload

--- ci4/app/Views/guest/success.php
+++ ci4/app/Views/guest/success.php
@@ -1,1 +1,256 @@
-<p>Guest entry created successfully.</p>
+<html>
+    <head>
+        <title> Website </title>
+        <link rel="stylesheet" href="style1.css"> </link>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <style>
+.error {color: #FF0000;}
+        body{
+            margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+}
+.banners{
+    width: 100%;
+    height: 100%;
+    background-image: url(images/background.png.jpg);
+    background-size: cover;
+    background-position: center;
+}
+
+html, body {
+  height: 100%;
+  margin: 0;
+  font-family: Lato;
+  color: rgb(123, 85, 85);
+}
+
+#page1 { background: #f6e0b5; }
+
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage1 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled1 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column1 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 50px;
+    padding: 10px;
+    width: 40%;
+    min-height: 500px;
+    background-size: 20px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h1 {
+  font-size: 20px;
+  font-style: italic;
+  position: relative;
+  margin-left: 25%;
+  margin-top: 10%;
+  color:rgb(118, 79, 79);
+  text-align: center;
+}
+
+p, a{
+  margin-bottom: 20%;
+  margin-right: 20%;
+  font-size: 20px;
+  font-style: normal;
+  color: #babc9a;
+  text-align: center;
+}
+
+
+.column2 {
+    float: right;
+    display: flex;
+    background: #dfe0cc;
+    margin-right: 50px;
+    padding: 10px;
+    width: 40%;
+    min-height: 500px;
+    border: 5px rgb(94, 68, 68) solid;
+}
+img {
+  width: 100%;
+  height: 100%;
+  margin-top: 70px;
+  display: block;
+  margin: auto;
+}
+
+
+#page2 { background: #efc1b0; }
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage2 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled2 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column3 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 5px;
+    margin-right: 0;
+    padding: 5px;
+    width: 97%;
+    max-height: 100%;
+    background-size: 5px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h2{
+  position: relative;
+  align-items: center;
+  text-align: center;
+}
+
+p, a{
+  position: relative;
+  text-align: center;
+}
+
+#page3 { background: #aa6f73; }
+
+.wrapper {
+  display: table;
+  height: 100%; width: 100%;
+  margin-top: 0%;
+}
+
+.stage3 {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.tabled, .middled3 {
+  text-align: center;
+  margin: 0 auto;
+}
+
+h1 { text-transform: uppercase; }
+h1 ~ span, p { color: #2d2d2d; }
+.thin { font-weight: 300; }
+.thick { font-weight: 900; }
+
+p {
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 1em;
+  font-size: 80%;
+}
+
+.column4 {
+    float: left;
+    display: flex;
+    background: #dfe0cc;
+    margin-left: 5px;
+    margin-right: 0;
+    padding: 5px;
+    width: 97%;
+    max-height: 100%;
+    background-size: 5px;
+    border: 5px rgb(94, 68, 68) solid
+    
+}
+
+h2{
+  position: relative;
+  align-items: center;
+  text-align: center;
+}
+
+p, a{
+  position: relative;
+  text-align: center;
+}
+
+
+
+</style>
+<div class="wrapper tabled">
+  <div class="stage2" id="page2">
+    <div class="middled2">
+      
+      <div class="row2">
+        <div class="column3">
+         
+           <p>&nbsp&nbsp&nbsp Guest entry created successfully.</p>
+
+          <div class="card">
+
+          
+
+
+           
+         </div>
+
+        </div>
+        </div>
+
+      
+
+    </div>
+  </div>
+</div>
+
+
+</html>
